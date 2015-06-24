2a443dcbf87b181bdfb600423b82cbc61b4cdfb4
goatstone <jose.collas@goatstone.com>
2015-06-24 10:19:44 -0700

    Modify POST function.

--- init.js
+++ init.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 		if(err){throw 'db error';}
 		var c = collection.find().limit(12).sort({timestamp:-1}).toArray(function(err, items){
 			var s = items.map(function(e, i){
-				return '<p>' + e.log.log + ' : ' + e.timestamp + '</p>';
+				return '<p>' + e.log.log.temperature + ' : ' + e.timestamp + '</p>';
 			});
 			// send the data to the client
 			res.send(s.join(" "));
@@ -23,12 +23,27 @@ app.get('/', function (req, res) {
 		});	
 	});
 });
-app.get('/pepple', function (req, res) {
-  addLog({log:'get'});
-  res.send('GET to pepple...');
+app.get('/pebble', function (req, res) {
+  var d = '';
+  req.on('data', function(c){
+  	d += c;
+  });
+  req.on('end', function(){
+  	console.log('end data..', req.query);
+  }) ; 
+  addLog({log:{metthod:'GET', temerature:99}});
+  res.send('GET ...');
 });
-app.post('/pepple', function (req, res) {
-  addLog({log:'post'});
+
+app.post('/pebble', function (req, res) {
+  var b = '';
+  req.on('data', function(d){
+  	b += d;
+  });
+  req.on('end', function(){
+  	var vals = b.split('=');
+    addLog({log:{temperature:vals[1]}});
+  }); 
   res.send('POST to pepple...');
 });
 var server = app.listen(3000, '192.168.1.7', function () {
